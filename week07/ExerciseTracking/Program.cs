81a223c481e05efc561b664f83137bdb5bc59c7b
Bryamtf <u20312070@utp.edu.pe>
2025-10-15 19:39:44 -0500

    activity's

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 4.8));
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 45, 15.0));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 25, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
